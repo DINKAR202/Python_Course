f52ae812f916bdf031b1d5c2194602bbe7438b3d
DINKAR202 <Dinkar202@outlook.com>
2024-01-25 23:19:51 +0530

    updated project

--- Django with mongodb/core/home/views.py
+++ Django with mongodb/core/home/views.py
@@ -7,10 +7,12 @@ def home(request):
         {'name':'Abhishek Gupta', 'age':'26'},
         {'name':'Ragni', 'age':'21'},
         {'name':'Neha kamti', 'age':'27'},
-        {'name':'Dinkar Raj', 'age':'19'},
+        {'name':'Dinkar Raj', 'age':'16'},
         {'name':'Preety singh', 'age':'22'}
     ]
-    return render(request, "home/index.html", context={'peoples':peoples})
+    
+    text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi vero veritatis qui beatae quidem aspernatur atque a deserunt eligendi ullam quos cumque corporis voluptatum voluptate incidunt eaque commodi, accusamus maxime ipsa voluptatem soluta natupudiandae perferendis odit. Facilis ipsum perferendis consectetur, odio repellendus veniam? Error ab labore ut optio aliquid sequi earum, doloribus accusantium dolores animi obcaecati qui id ipsum dignissimos sunt ipsam? Nisi rem cum voluptas!"
+    return render(request, "home/index.html", context={'peoples':peoples, 'text':text})
     
 
 def about(request):
