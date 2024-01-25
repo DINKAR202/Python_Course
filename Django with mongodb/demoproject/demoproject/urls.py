978bd02c54bafafbc77aad5e3171dcb6fc0e2023
DINKAR202 <Dinkar202@outlook.com>
2024-01-25 21:04:54 +0530

    updated project

--- Django with mongodb/demoproject/demoproject/urls.py
+++ Django with mongodb/demoproject/demoproject/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import path
+from project1 import views
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
-    path('', include('members.urls')),
 ]
--- Django with mongodb/demoproject/project1/views.py
+++ Django with mongodb/demoproject/project1/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
 from django.http import HttpResponse
 
-def members(request):
-    return HttpResponse("Hello world!")
+# Create your views here.
+def home(request):
+    return HttpResponse("Hey i'm a django server.")
