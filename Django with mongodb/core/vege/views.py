526c80169961bb7c10e6e0a7bbf3ea3d587e1a7a
DINKAR202 <Dinkar202@outlook.com>
2024-01-26 14:50:23 +0530

    updated project

--- Django with mongodb/core/vege/views.py
+++ Django with mongodb/core/vege/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
 def receipes(request):
     if request.method == "POST":
         data = request.POST
@@ -8,8 +9,10 @@ def receipes(request):
         receipe_name = data.get('receipe_name')
         receipe_description = data.get('receipe_description')
         
-        print(receipe_name)
-        print(receipe_description)
-        print(receipe_image)
+        Receipe.objects.create(
+            receipe_image = receipe_image,
+            receipe_name = receipe_name,
+            receipe_description = receipe_description,
+        )
         
     return render(request, 'receipes.html')
