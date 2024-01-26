211057a6ae1e96e89fe0b321bd151437345c485c
DINKAR202 <Dinkar202@outlook.com>
2024-01-26 20:35:31 +0530

    updated project

--- Django with mongodb/core/core/settings.py
+++ Django with mongodb/core/core/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 import os
 
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIR = {
--- Django with mongodb/core/core/urls.py
+++ Django with mongodb/core/core/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from home.views import *
 from vege.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home, name='home'),
@@ -15,3 +18,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
+urlpatterns += staticfiles_urlpatterns() 
