7bd1c9fe1cc3140130c8127190975462b7d80dbd
DINKAR202 <Dinkar202@outlook.com>
2024-01-26 13:30:33 +0530

    updated project

--- Django with mongodb/core/home/models.py
+++ Django with mongodb/core/home/models.py
@@ -6,12 +6,10 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField(null=True, blank=True)
-    address = models.TextField(null=True, blank=True)
-    # image = models.ImageField()
-    # file = models.FileField()
+    address = models.TextField(null=True, blank=True)    
     
-    
-class Product(models.Model):
-    pass    
+class Car(models.Model):
+    car_name = models.CharField(max_length=500)
+    speed = models.IntegerField(default=50)  
     
     
