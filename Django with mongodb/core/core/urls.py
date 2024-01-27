45a3c7ee47b2cf6d7d2ee3dbe6fa84546055aa0f
DINKAR202 <Dinkar202@outlook.com>
2024-01-28 01:09:13 +0530

    updated project

--- Django with mongodb/core/core/urls.py
+++ Django with mongodb/core/core/urls.py
@@ -1,3 +1,33 @@
+# from django.contrib import admin
+# from django.urls import path
+# from home.views import *
+# from vege.views import *
+# from django.conf.urls.static import static
+# from django.conf import settings
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# urlpatterns = [
+#     path('', home, name='home'),
+    
+#     path('receipes/', receipes, name='receipes'),
+    
+#     path('delete-receipe/<id>/', delete_receipe, name="delete_receipe"),
+#     path('update-receipe/<id>/', update_receipe, name="update_receipe"),
+    
+#     path('about/', about, name='about'),
+#     path('contact/', contact, name='contact'),
+    
+#     path('admin/', admin.site.urls),
+# ]
+
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, 
+#                           document_root=settings.MEDIA_ROOT)
+    
+
+# urlpatterns += staticfiles_urlpatterns() 
+
 from django.contrib import admin
 from django.urls import path
 from home.views import *
@@ -11,8 +41,8 @@ urlpatterns = [
     
     path('receipes/', receipes, name='receipes'),
     
-    path('delete-receipe/<id>/', delete_receipe, name="delete_receipe"),
-    path('update-receipe/<id>/', update_receipe, name="update_receipe"),
+    path('delete-receipe/<int:id>/', delete_receipe, name="delete_receipe"),
+    path('update-receipe/<int:id>/', update_receipe, name="update_receipe"),
     
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
@@ -20,10 +50,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, 
-                          document_root=settings.MEDIA_ROOT)
-    
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += staticfiles_urlpatterns() 
+urlpatterns += staticfiles_urlpatterns()
--- Django with mongodb/core/vege/views.py
+++ Django with mongodb/core/vege/views.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render, redirect
-from .models import *
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+# from django.shortcuts import render, redirect
+# from .models import *
+# from django.http import HttpResponse
+# from django.shortcuts import get_object_or_404
+# # Create your views here.
 
-# Create your views here.
+# def receipes(request):
+#     if request.method == "POST":
+#         data = request.POST
+#         receipe_name = data.get('receipe_name')
+#         receipe_description = data.get('receipe_description')
+#         receipe_image = request.FILES.get('receipe_image')
+        
+#         Receipe.objects.create(
+#             receipe_name = receipe_name,
+#             receipe_description = receipe_description,
+#             receipe_image = receipe_image,
+#         )
+        
+#         return redirect('/receipes/')
+    
+#     queryset = Receipe.objects.all()
+#     context = {'receipes': queryset}   
+#     return render(request, 'receipes.html', context)
+
+# def update_receipe(request, id):
+#     queryset = Receipe.objects.get(id = id)
+#     context = {'receipe': queryset}   
+    
+#     return render(request, 'update_receipes.html', context)
+    
+
+
+# def delete_receipe(request, id):
+#     queryset = Receipe.objects.get(id = id)
+#     print("ID", queryset)
+#     queryset.delete()
+#     return redirect('/receipes/')
+    
+    
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Receipe
 
 def receipes(request):
     if request.method == "POST":
@@ -13,9 +49,9 @@ def receipes(request):
         receipe_image = request.FILES.get('receipe_image')
         
         Receipe.objects.create(
-            receipe_name = receipe_name,
-            receipe_description = receipe_description,
-            receipe_image = receipe_image,
+            receipe_name=receipe_name,
+            receipe_description=receipe_description,
+            receipe_image=receipe_image,
         )
         
         return redirect('/receipes/')
@@ -25,15 +61,11 @@ def receipes(request):
     return render(request, 'receipes.html', context)
 
 def update_receipe(request, id):
-    queryset = Receipe.objects.get(id = id)
-    context = {'receipe': queryset}   
-    
+    receipe = get_object_or_404(Receipe, id=id)
+    context = {'receipe': receipe}   
     return render(request, 'update_receipes.html', context)
-    
-
 
 def delete_receipe(request, id):
-    queryset = Receipe.objects.get(id = id)
-    queryset.delete()
+    receipe = get_object_or_404(Receipe, id=id)
+    receipe.delete()
     return redirect('/receipes/')
-    
