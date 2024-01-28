f24521a9247c3d130ec568e6419e88fd2e30b326
DINKAR202 <Dinkar202@outlook.com>
2024-01-29 00:21:03 +0530

    updated project

--- Django with mongodb/core/vege/views.py
+++ Django with mongodb/core/vege/views.py
@@ -51,14 +51,14 @@ def delete_receipe(request, id):
     
 def login_page(request):
     if request.method == "POST":
-        username = request.POST.get('username')
+        email = request.POST.get('email')
         password = request.POST.get('password')
         
-        if User.objects.filter(username = username).exists():
-            messages.error(request, 'Invalid Username')
+        if not User.objects.filter(email = email).exists():
+            messages.error(request, 'Invalid email')
             return redirect('/login/')
         
-        user = authenticate(username = username, password = password)
+        user = authenticate(email = email, password = password)
         
         if user is None:
             messages.error(request, 'Invalid Password')
