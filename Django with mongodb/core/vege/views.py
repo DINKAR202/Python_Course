f96b96b0f7e928b75e9daad6f9aa34465d717256
DINKAR202 <Dinkar202@outlook.com>
2024-01-28 23:59:50 +0530

    updated project

--- Django with mongodb/core/vege/views.py
+++ Django with mongodb/core/vege/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url="/login/")
 def receipes(request):
     if request.method == "POST":
         data = request.POST
