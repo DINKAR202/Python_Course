9e3c6dea81f3ec2c050198ecf3967311135dcfc0
DINKAR202 <Dinkar202@outlook.com>
2024-01-31 22:55:20 +0530

    updated project

--- Dreamproject2/customadmin/models.py
+++ Dreamproject2/customadmin/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class POST(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+class Postcategory(models.Model):
+    cat_content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    
