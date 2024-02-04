4e749a0fd2d069c96a9f97539de7110868e0dc32
DINKAR202 <Dinkar202@outlook.com>
2024-02-04 20:49:37 +0530

    updated project

--- centralizedproject/students/views.py
+++ centralizedproject/students/views.py
@@ -34,7 +34,6 @@ def login_page(request):
         
         else:
             login(request, user)
-            # return redirect('/receipes/')
             return redirect('/student-dashboard/')
     
     context = {
@@ -53,6 +52,9 @@ def sign_up(request):
         first_name = request.POST.get('first_name')
         last_name = request.POST.get('last_name')
         username = request.POST.get('username')
+        date_of_birth = request.POST.get('date_of_birth')
+        phone_number = request.POST.get('phone_number')
+        address = request.POST.get('address')
         email = request.POST.get('email')
         password = request.POST.get('password')
         
@@ -70,6 +72,9 @@ def sign_up(request):
         user = User.objects.create(
             first_name = first_name,
             last_name = last_name,
+            date_of_birth = date_of_birth,
+            phone_number = phone_number,
+            address = address,
             username = username,
             email = email,
         )
