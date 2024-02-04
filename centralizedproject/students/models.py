ca9c0e57d5eac84e927ef06a60f553601c9f8700
DINKAR202 <Dinkar202@outlook.com>
2024-02-04 20:45:40 +0530

    updated project

--- centralizedproject/students/models.py
+++ centralizedproject/students/models.py
@@ -6,3 +6,12 @@ from django.contrib.auth.models import User
 
 class Student(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null = True , blank = True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField()
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
