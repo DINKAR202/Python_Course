1afc7af3cbdd5374c24c6aca8f3b95a583782b23
DINKAR202 <Dinkar202@outlook.com>
2024-02-13 23:03:51 +0530

    updated project

--- educationsystem/educationsystem/settings.py
+++ educationsystem/educationsystem/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,19 @@ WSGI_APPLICATION = 'educationsystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+       'default': {
+           'ENGINE': 'djongo',
+           'NAME': 'education',
+       }
+   }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
