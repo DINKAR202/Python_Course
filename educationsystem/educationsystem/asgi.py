d85ed7250c9329d5c2b3516397250122853394f4
DINKAR202 <Dinkar202@outlook.com>
2024-02-13 22:59:00 +0530

    updated project

--- educationsystem/educationsystem/asgi.py
+++ educationsystem/educationsystem/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for centralizedproject project.
+ASGI config for educationsystem project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'centralizedproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educationsystem.settings')
 
 application = get_asgi_application()
--- educationsystem/educationsystem/wsgi.py
+++ educationsystem/educationsystem/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for centralizedproject project.
+WSGI config for educationsystem project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'centralizedproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educationsystem.settings')
 
 application = get_wsgi_application()
