85555ae6b7d934c8398e468bd37109d2864eeebb
DINKAR202 <Dinkar202@outlook.com>
2024-02-13 23:17:00 +0530

    updated project

--- educationsystem/student/models.py
+++ educationsystem/student/models.py
@@ -1,6 +1,21 @@
 from djongo import models
+from djongo.models import User
+from django.conf import settings
+from djongo.models import DjongoManager as CustomManager
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
-class Blog(models.Model):
-    name = models.CharField(max_length=100)
-    tagline = models.TextField()
+class CustomUser(AbstractUser):
+        username = None
+        email = models.EmailField("email_address", unique=True)   
+        USERNAME_FIELD = "email"
+        REQUIRED_FIELDS = []
+        objects = CustomManager()
+        
+        def __str__(self):
+            return self.email
+
+
+class Student(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
